7676c973f8623693f715162c68594e277d52cd0c
vyonar <veyselyonar@yahoo.com>
2025-09-13 10:07:14 +0300

    tewtw

--- config/config.ts
+++ config/config.ts
@@ -166,9 +166,7 @@ export default defineConfig({
       projectName: 'swagger',
     },
   ],
-  mock: {
-    include: ['mock/**/*', 'src/pages/**/_mock.ts'],
-  },
+  mock: false,
   /**
    * @name 是否开启 mako
    * @description 使用 mako 极速研发
--- config/proxy.ts
+++ config/proxy.ts
@@ -4,8 +4,6 @@ export default {
       target: 'https://mais-service-1002606543988.europe-west1.run.app',
       changeOrigin: true,
       secure: true,
-      // backend zaten /api ile başlıyor, pathRewrite gerekmez
-      // pathRewrite: { '^/api': '' },
     },
   },
   // test/prod ortamları için istersen benzer bloklar ekleyebilirsin
